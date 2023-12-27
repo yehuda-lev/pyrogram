8cc9ec991ae4fb6d3f78e5510e1e2917b0bb2933
shriMADhav U k <hetzner@shrimadhavuk.me>
2023-12-27 15:23:31 +0100

    Fix Layer 167 reserved keyword condition check

--- compiler/api/compiler.py
+++ compiler/api/compiler.py
@@ -258,10 +258,13 @@ def start(format: bool = False):
 
             args = ARGS_RE.findall(line)
 
-            # Fix arg name being "self" (reserved python keyword)
+            # Fix arg name being reserved python keyword
             for i, item in enumerate(args):
-                if item[0] == "self":
-                    args[i] = ("is_self", item[1])
+                if item[0] in [
+                    "self",
+                    "from",
+                ]:
+                    args[i] = (f"is_{item[0]}", item[1])
 
             combinator = Combinator(
                 section=section,
