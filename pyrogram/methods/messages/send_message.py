bf8ef88b9a91cd302d50eabdda3170fcdf0b65b1
shriMADhav U k <hetzner@shrimadhavuk.me>
2024-01-17 09:04:15 +0100

    Fix condition for sending message

--- pyrogram/methods/messages/send_message.py
+++ pyrogram/methods/messages/send_message.py
@@ -137,7 +137,31 @@ class SendMessage:
         )
         message, entities = (await utils.parse_text_entities(self, text, parse_mode, entities)).values()
 
-        if message:
+        if (
+            link_preview_options and
+            link_preview_options.url
+        ):
+            r = await self.invoke(
+                raw.functions.messages.SendMedia(
+                    peer=await self.resolve_peer(chat_id),
+                    silent=disable_notification or None,
+                    reply_to=reply_to,
+                    random_id=self.rnd_id(),
+                    schedule_date=utils.datetime_to_timestamp(schedule_date),
+                    reply_markup=await reply_markup.write(self) if reply_markup else None,
+                    message=message,
+                    media=raw.types.InputMediaWebPage(
+                        url=link_preview_options.url,
+                        force_large_media=link_preview_options.prefer_large_media,
+                        force_small_media=link_preview_options.prefer_small_media
+                    ),
+                    invert_media=link_preview_options.show_above_text,
+                    entities=entities,
+                    noforwards=protect_content
+                )
+            )
+
+        elif message:
             r = await self.invoke(
                 raw.functions.messages.SendMessage(
                     peer=await self.resolve_peer(chat_id),
@@ -157,25 +181,13 @@ class SendMessage:
                     entities=entities,
                 )
             )
-        elif link_preview_options.url:
-            r = await self.invoke(
-                raw.functions.messages.SendMedia(
-                    peer=await self.resolve_peer(chat_id),
-                    silent=disable_notification or None,
-                    reply_to=reply_to,
-                    random_id=self.rnd_id(),
-                    schedule_date=utils.datetime_to_timestamp(schedule_date),
-                    reply_markup=await reply_markup.write(self) if reply_markup else None,
-                    message=message,
-                    media=raw.types.InputMediaWebPage(
-                        url=link_preview_options.url,
-                        force_large_media=link_preview_options.prefer_large_media,
-                        force_small_media=link_preview_options.prefer_small_media
-                    ),
-                    invert_media=link_preview_options.show_above_text,
-                    entities=entities,
-                    noforwards=protect_content
-                )
+
+        else:
+            # TODO
+            return types.Message(
+                id=0,
+                # TODO
+                date=utils.timestamp_to_datetime(0),
             )
 
         if isinstance(r, raw.types.UpdateShortSentMessage):
