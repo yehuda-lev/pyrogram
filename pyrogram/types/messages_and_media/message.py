yehuda-lev/pyrogram

commit a8ec0701c59dd7d0e5c9f792df8b2bbde752ef48

Fix bug in parsing TextQuote and ExternalReplyInfo

pyrogram/types/messages_and_media/message.py
@@ -1226,13 +1226,12 @@ class Message(Object, Update):
                         parsed_message.message_thread_id = message.reply_to.reply_to_top_id
                     else:
                         parsed_message.message_thread_id = message.reply_to.reply_to_msg_id
-                if message.reply_to.quote:
-                    parsed_message.quote = types.TextQuote._parse(
-                        client,
-                        chats,
-                        users,
-                        message.reply_to
-                    )
+                parsed_message.quote = types.TextQuote._parse(
+                    client,
+                    chats,
+                    users,
+                    message.reply_to
+                )
 
             if isinstance(message.reply_to, raw.types.MessageReplyStoryHeader):
                 parsed_message.reply_to_story = await types.Story._parse(client, chats, None, message.reply_to)

pyrogram/types/input_message_content/external_reply_info.py
@@ -169,7 +169,14 @@ class ExternalReplyInfo(Object):
                 users,
                 chats,
             )
+
             chat = None
+            if isinstance(reply_to.reply_to_peer_id, raw.types.PeerChannel):
+                raw_peer_id = utils.get_raw_peer_id(reply_to.reply_to_peer_id)
+                chat = types.Chat._parse_chat(
+                    client,
+                    chats[raw_peer_id],
+                )
 
             animation = None
             audio = None

pyrogram/types/input_message_content/text_quote.py
@@ -65,13 +65,10 @@ class TextQuote(Object):
         users: dict,
         reply_to: "raw.types.MessageReplyHeader"
     ) -> "TextQuote":
-        if not getattr(reply_to, "quote", None):
-            return None
-
         if isinstance(reply_to, raw.types.MessageReplyHeader):
             quote_text = reply_to.quote_text
             quote_entities = reply_to.quote_entities
-            position = reply_to.quote_offset
+            position = reply_to.quote_offset or 0
 
             entities = [
                 types.MessageEntity._parse(client, entity, users)
@@ -85,5 +82,5 @@ class TextQuote(Object):
                 text=Str(quote_text).init(entities) or None,
                 entities=entities,
                 position=position,
-                # TODO
+                is_manual=bool(reply_to.quote) or None
             )

commit e9167ec8df2ef4d92e432f1f416c9ea33e4a2797

Fix bug in TextQuote parsing
Regression was introduced in a8ec070

pyrogram/types/messages_and_media/message.py
@@ -1235,12 +1235,13 @@ class Message(Object, Update):
                         parsed_message.message_thread_id = message.reply_to.reply_to_top_id
                     else:
                         parsed_message.message_thread_id = message.reply_to.reply_to_msg_id
-                parsed_message.quote = types.TextQuote._parse(
-                    client,
-                    chats,
-                    users,
-                    message.reply_to
-                )
+                if getattr(message.reply_to, "quote", False):
+                    parsed_message.quote = types.TextQuote._parse(
+                        client,
+                        chats,
+                        users,
+                        message.reply_to
+                    )
 
             if isinstance(message.reply_to, raw.types.MessageReplyStoryHeader):
                 parsed_message.reply_to_story = await types.Story._parse(client, chats, None, message.reply_to)